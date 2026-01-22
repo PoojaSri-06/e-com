3692ad51f9553c82f66ceb439e68c341f88ad6fa
poojasri-06 <poojasrilakshmibhavani@gmail.com>
2026-01-22 14:26:36 +0530

    updated with navbar

--- firstapp/src/components/Register.jsx
+++ firstapp/src/components/Register.jsx
@@ -1,15 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Register() {
-  const[name,setName]= useState("")
-  const[email,setEmail]=useState("")
-  const[password,setPassword]=useState("")
-  const[address,setAddress]=useState("")
-  const[mobile,setMobile]=useState()
-  function handleRegister(e){
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [address, setAddress] = useState("")
+  const [mobile, setMobile] = useState()
+
+
+  function handleRegister(e) {
     e.preventDefault()
-    console.log(e)
+    // console.log(e)
+    let newUser = { name, password, email, address, mobile }
+    console.log(newUser)
+    setName("")
+    setEmail("")
+    setPassword("")
+    setAddress("")
+    setMobile("")
+
+
   }
+
+
   return (
     <div className='container mt-4'>
       <div className='row'>
@@ -17,19 +30,19 @@ export default function Register() {
           <h2>Register</h2>
           <div className='mb-3'>
             <label className="form-label">Name</label>
-            <input type="text" className="form-control" name="name" />
+            <input type="text" className="form-control" name="name" value={name} onChange={(e) => setName(e.target.value)} />
           </div>
           <div className='mb-3'>
             <label className="form-label">Email</label>
-            <input type="text" className="form-control" name="email" />
+            <input type="text" className="form-control" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className='mb-3'>
             <label className="form-label">Password</label>
-            <input type="text" className="form-control" name="password" />
+            <input type="text" className="form-control" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className='mb-3'>
             <label className="form-label">Address</label>
-            <input type="text" className="form-control" name="address" />
+            <input type="text" className="form-control" name="address" value={address} onChange={(e) => setAddress(e.target.value)} />
           </div>
           <div className='mb-3'>
             <button className='btn btn-outline-success btn-lg'>Submit</button>
